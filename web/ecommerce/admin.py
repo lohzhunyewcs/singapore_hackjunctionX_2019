7a74569022b904ebb3646221d45419aa03e21c5b
Jia Wen Chong <jiawchong@gmail.com>
2019-09-28 07:17:42 +0800

    Added sub-categories and sample file path

--- web/ecommerce/admin.py
+++ web/ecommerce/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from ecommerce.models import Post
+from ecommerce.models import Post, Category, SubCategory, FinalCategory, Product, Seller, Item
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(FinalCategory)
+admin.site.register(Product)
+admin.site.register(Seller)
+admin.site.register(Item)
--- web/ecommerce/models.py
+++ web/ecommerce/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Post(models.Model):
@@ -16,13 +17,26 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class SubCategory(models.Model):
+    name = models.CharField(max_length = 100)
+    parent_cat = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    def __str__(self):
+        return self.name
+
+class FinalCategory(models.Model):
+    name = models.CharField(max_length = 100)
+    parent_cat = models.ForeignKey(SubCategory, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.name
+
 # The taken photo from webcam = PRODUCT
 class Product(models.Model):
     name = models.CharField(max_length = 100)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(FinalCategory, on_delete=models.SET_NULL, null=True)
 
 ## ** ## ** TODO: Fill up upload_to which saves the taken photo onto the directory
-    photo = models.FileField(upload_to = "")
+    photo = models.FileField(upload_to = "ecommerce/img/")
 
     def __str__(self):
         return self.name
@@ -36,16 +50,18 @@ class Seller(models.Model):
 # The identified item based on the PRODUCT photo == ITEM
 class Item(models.Model):
     name = models.CharField(max_length = 100)
-    discount = models.IntegerField()            # No discount if value == 0
+    discount = models.DecimalField(max_digits=3, decimal_places=2, validators=[MinValueValidator(0), MaxValueValidator(1.0)])            # No discount if value == 0 #changed from IntegerField to DecimalField with validators
     price = models.DecimalField(max_digits=10, decimal_places=2)
     rating = models.IntegerField() 
     soldUnits = models.IntegerField()
+    
 
 ## ** ## ** ## TODO: Fill up the file path where the item photos are stored, match takes a regEx (eg: foo(a-zA-Z)*), recursive == TRUE checks the subdirectory
-    photo = models.FilePathField(path="",match="",recursive=)
+    photo = models.FilePathField(path="ecommerce/img/",match=r'pencil',recursive=False)
 
-    prodInferred = models.ForeignKey(Product, on_delete=models.CASCADE) # The taken photo on webcam which infers to this item
+    prodInferred = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True) # The taken photo on webcam which infers to this item
     seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.name
+
